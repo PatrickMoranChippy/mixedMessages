ac888e11efcfbdf547bd6e6a94c85ff8145b411a
Patrick Moran <morancc@outlook.com>
2023-07-05 16:32:49 +1000

    Initialised first quote structure object

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Start of MixedMessages Project.
+
+let quoteStructure = [{}, {}, {}];   //Initialise array of three empty objects.
+
+quoteStructure[0] = {
+    _structure: "1 to 2 each 3 your 4.",
+    _firstSigWord: ["Try", "Strive", "Endeavor"],
+    _secondSigWord: ["make", "create", "envision"],
+    _thirdSigWord: ["day", "week", "project"],
+    _forthSigWord: ["masterpiece", "magnum opus", "piece de resistance"]
+}
+
